f0f75fb6d4180dcf5a1ea03035389b1b9126fbb3
Verbon <verbon@outlook.com>
2017-04-20 23:50:05 +0300

    Gulp build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,15 @@
 var gulp = require('gulp');
 var deleteLines = require('gulp-delete-lines');
+var run = require('gulp-run');
+
+gulp.task('compile-electron', function() { 
+    return run('tsc -p src/tsconfig.json').exec();
+});
+
+gulp.task('build-angular', ['compile-electron'], function() {
+    return run('webpack').exec();
+});
+
+gulp.task('default', ['build-angular'], function() {
+
+});
