77da71f9d8957b1ae7ad1dd72599ed7210d20869
Verbon <verbon@outlook.com>
2017-04-30 16:22:47 +0300

    Add gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var deleteLines = require('gulp-delete-lines');
 var run = require('gulp-run');
+var clean = require('gulp-clean');
 
-gulp.task('compile-electron', function() { 
+gulp.task('clean', function() {
+    return gulp.src('./bin', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('compile-electron', ['clean'], function() { 
     return run('tsc -p src/tsconfig.json').exec();
 });
 
