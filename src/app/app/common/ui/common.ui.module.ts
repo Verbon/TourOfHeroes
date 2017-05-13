806f998257dc78ebae537a58a61dde4a41a18402
Verbon <verbon@outlook.com>
2017-05-13 13:37:06 +0300

    Slide down transitions

--- src/app/app/common/ui/common.ui.module.ts
+++ src/app/app/common/ui/common.ui.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { KebabPipe } from './string-transforms/kebab.pipe';
 
@@ -12,6 +13,7 @@ import { KebabPipe } from './string-transforms/kebab.pipe';
     exports: [
         CommonModule,
         FormsModule,
+        BrowserAnimationsModule,
         KebabPipe
     ]
 })
