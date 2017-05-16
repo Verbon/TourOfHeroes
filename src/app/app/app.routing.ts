bd09449b37b15abd5e5734ea96c7d8b00c21fce8
Uladzislau Narutsky <uladzislau.narutsky@itechart-group.com>
2017-05-16 17:01:46 +0300

    Change absolute routing to relative

--- src/app/app/app.routing.ts
+++ src/app/app/app.routing.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-    { path: '', redirectTo: '/heroes', pathMatch: 'full' }
+    { path: '', redirectTo: 'heroes', pathMatch: 'full' }
 ];
 
 @NgModule({
